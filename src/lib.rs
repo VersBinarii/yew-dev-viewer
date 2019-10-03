23ced96279c099ef7f08e2cf1aaf92582e53377c
Kris <kristhesavage@gmail.com>
2019-10-03 14:08:06 +0200

    Cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,39 @@
 #![recursion_limit = "512"]
 
 mod device;
-mod devices;
 mod device_modal;
+mod devices;
 
-use stdweb::web::Date;
-use yew::services::ConsoleService;
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
 
 use crate::devices::Devices;
 
-pub struct RootModel {
-  console: ConsoleService,
-}
+pub struct RootModel;
 
 impl Component for RootModel {
-  type Message = ();
-  type Properties = ();
+    type Message = ();
+    type Properties = ();
 
-  fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-    RootModel {
-      console: ConsoleService::new(),
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        RootModel {}
     }
-  }
 
-  fn update(&mut self, msg: Self::Message) -> ShouldRender {
-    true
-  }
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
 }
 
 impl Renderable<RootModel> for RootModel {
-  fn view(&self) -> Html<Self> {
-    html! {
-        <section class="section">
-            <div class="continer">
-            <h2 class="title">
-              {"Fjord Telecom nodes"}
-            </h2>
-            <Devices />
-            </div>
-        </section>
+    fn view(&self) -> Html<Self> {
+        html! {
+            <section class="section">
+                <div class="continer">
+                <h2 class="title">
+                  {"My nodes"}
+                </h2>
+                <Devices />
+                </div>
+            </section>
+        }
     }
-  }
 }
